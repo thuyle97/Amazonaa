3a5e869d61665d67c208f8827e18e7234d763e84
Thuy Le <phuongthuy9796@gmail.com>
2023-10-25 20:45:28 -0700

    Add rating and review to product comp

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Rating from "./Rating";
 
 function Product(props) {
   const { product } = props;
