003b10e2331f706a0a331ddee095efe5bf4f3f1e
Thuy Le <phuongthuy9796@gmail.com>
2023-10-25 20:22:40 -0700

    Add Product component

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+function Product(props) {
+  const { product } = props;
+  return (
+    <div className="product" key={product.slug}>
+      <Link to={`/product/${product.slug}`}>
+        <img src={product.image} alt={product.name}></img>
+      </Link>
+      <div className="product-info">
+        <Link to={`/product/${product.slug}`}>
+          <p>{product.name}</p>
+        </Link>
+        <p>
+          <strong>${product.price}</strong>
+        </p>
+        <button>Add to cart</button>
+      </div>
+    </div>
+  );
+}
+export default Product;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,9 @@
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useReducer } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import logger from "use-reducer-logger";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Product from "../components/Product";
 
 //Create the reducer here:
 const reducer = (state, action) => {
@@ -45,22 +47,13 @@ function HomeScreen() {
         ) : error ? (
           <div>{error}</div>
         ) : (
-          products.map((product) => (
-            <div className="product" key={product.slug}>
-              <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name}></img>
-              </Link>
-              <div className="product-info">
-                <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))
+          <Row>
+            {products.map((product) => (
+              <Col sm={6} md={4} lg={12} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
         )}
       </div>
     </div>
