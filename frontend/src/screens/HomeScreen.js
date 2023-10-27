8ba760d208c17b0d0e451d4a206ddfa0680e7297
Thuy Le <phuongthuy9796@gmail.com>
2023-10-26 22:21:17 -0700

    add messagebox and loading box

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,6 +4,8 @@ import logger from "use-reducer-logger";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Product from "../components/Product";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
 //Create the reducer here:
 const reducer = (state, action) => {
@@ -43,9 +45,9 @@ function HomeScreen() {
       <h2>Featured Products</h2>
       <div className="products">
         {loading ? (
-          <div>Loading...</div>
+          <LoadingBox />
         ) : error ? (
-          <div>{error}</div>
+          <MessageBox variant="danger">{error}</MessageBox>
         ) : (
           <Row>
             {products.map((product) => (
