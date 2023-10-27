5e9cf0398457a6bd6f91b99380f610bacfdd6bdf
Thuy Le <phuongthuy9796@gmail.com>
2023-10-26 22:40:16 -0700

    Add initial state in store

---
+++ frontend/src/Store.js
@@ -0,0 +1,15 @@
+import { createContext, useReducer } from "react";
+
+export const Store = createContext();
+
+const initialState = {
+  cart: {
+    cartItems: [],
+  },
+};
+
+export function StoreProvider(props) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const value = { state, dispatch };
+  return <Store.Provider value={value}>{props.children}</Store.Provider>;
+}
