9a20ccc6e6b1f8e64bc5d093a5a34a662eb866b7
Thuy Le <phuongthuy9796@gmail.com>
2023-10-27 18:55:02 -0700

    finish cart screen ui

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -76,6 +76,17 @@ export default function CartScreen() {
                     {cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}
                   </h3>
                 </ListGroup.Item>
+                <ListGroup.Item>
+                  <div className="d-grid">
+                    <Button
+                      type="button"
+                      variant="primary"
+                      disabled={cartItems.length === 0}
+                    >
+                      Proceed to CheckOut
+                    </Button>
+                  </div>
+                </ListGroup.Item>
               </ListGroup>
             </Card.Body>
           </Card>
