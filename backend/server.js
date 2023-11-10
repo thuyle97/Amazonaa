98a01aef92ae3fff2caf2e3fb383b3871ba2ccb7
Thuy Le <phuongthuy9796@gmail.com>
2023-11-09 21:47:34 -0800

    Complete creating sample products

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import data from "./data.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -17,28 +18,7 @@ mongoose
 
 const app = express();
 app.use("/api/seed", seedRouter);
-
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
-
-app.get("/api/products/slug/:slug", (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
+app.use("/api/products", productRouter);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
