b27d014407826209c208aea0c92d706c78b7e175
Thuy Le <phuongthuy9796@gmail.com>
2023-11-15 20:04:49 -0800

    Create sample users complete

--- backend/data.js
+++ backend/data.js
@@ -1,9 +1,17 @@
+import bcrypt from "bcryptjs";
 const data = {
   users: [
     {
       name: "Thuy",
       email: "admin@example.com",
       password: bcrypt.hashSync("123456"),
+      isAdmin: true,
+    },
+    {
+      name: "John",
+      email: "user@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: false,
     },
   ],
   products: [
