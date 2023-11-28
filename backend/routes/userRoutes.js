090f852a5f2f001e94b2da2a7e327cfa6da3d40b
Thuy Le <phuongthuy9796@gmail.com>
2023-11-27 23:22:40 -0800

    add sign-in function

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,24 @@
 import express from "express";
+import User from "../models/userModel";
 
 const userRouter = express.Router();
 
 userRouter.post(
   "/signin",
-  expressAsyncHandler(async (req, res) => {})
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+        });
+        return;
+      }
+    }
+    res.status(401).send({ message: "Invalid email or password." });
+  })
 );
