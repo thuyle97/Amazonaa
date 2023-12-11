fe1e2c38642f0919fe4d5fdeb58429369ed5a425
Thuy Le <phuongthuy9796@gmail.com>
2023-12-10 22:00:27 -0800

    add reducer function to order screen

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -2,6 +2,19 @@ import React, { useReducer } from "react";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true, error: "" };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: true, order: action.payload, error: "" };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+}
+
 export default function OrderScreen() {
   const [{ loading, error, order }, dispatch] = useReducer(reducer, {
     loading: true,
