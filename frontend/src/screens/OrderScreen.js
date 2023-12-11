06002ec620cb93ef3dc2d2f280955dc9e30e072b
Thuy Le <phuongthuy9796@gmail.com>
2023-12-10 22:23:25 -0800

    add fetch order function

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,6 +1,10 @@
-import React, { useReducer } from "react";
+import React, { useContext, useEffect, useReducer } from "react";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
+import { Store } from "../Store";
+import { useNavigate, useParams } from "react-router-dom";
+import { getError } from "../utils";
+import axios from "axios";
 
 function reducer(state, action) {
   switch (action.type) {
@@ -16,11 +20,40 @@ function reducer(state, action) {
 }
 
 export default function OrderScreen() {
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+
+  const params = useParams();
+  const { id: orderId } = params;
+  const navigate = useNavigate();
+
   const [{ loading, error, order }, dispatch] = useReducer(reducer, {
     loading: true,
     order: {},
     error: "",
   });
+
+  useEffect(() => {
+    const fetchOrder = async () => {
+      try {
+        dispatch({ type: "FETCH_REQUEST" });
+        const { data } = await axios.get(`api/orders/${orderId}`, {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        });
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: getError(err) });
+      }
+    };
+
+    if (!userInfo) {
+      return navigate("/login");
+    }
+    if (!order._id || (order._id && order._id !== orderId)) {
+      fetchOrder();
+    }
+  }, [order, userInfo, orderId, navigate]);
+
   return loading ? (
     <LoadingBox></LoadingBox>
   ) : error ? (
