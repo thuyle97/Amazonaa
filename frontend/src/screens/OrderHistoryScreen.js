5edb7bfb437eaddc2a736dbd904c165897d9e993
Thuy Le <phuongthuy9796@gmail.com>
2023-12-21 23:11:05 -0800

    Add order history screen

---
+++ frontend/src/screens/OrderHistoryScreen.js
@@ -0,0 +1,54 @@
+import React, { useContext, useReducer } from "react";
+import { Helmet } from "react-helmet-async";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { Store } from "../Store";
+import { useNavigate } from "react-router-dom";
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_REQUEST':
+            return {...state, loading: true};
+        case 
+    }
+}
+
+export default function OrderHistoryScreen() {
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+  const navigate = useNavigate();
+
+  const [{ loading, error, orders }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: "",
+  });
+
+  return (
+    <div>
+      <Helmet>
+        <title>Order History</title>
+      </Helmet>
+
+      <h1>Order History</h1>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>DATE</th>
+              <th>TOTAL</th>
+              <th>PAID</th>
+              <th>DELIVERED</th>
+              <th>ACTIONS</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      )}
+    </div>
+  );
+}
