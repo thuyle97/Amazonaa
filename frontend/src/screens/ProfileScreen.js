5c89154a17c7baf69700596719f45b5825701a60
Thuy Le <phuongthuy9796@gmail.com>
2024-01-19 00:13:56 -0800

    Add form into user profile page

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,15 +1,55 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Store } from "../Store";
 import { Helmet } from "react-helmet-async";
+import Form from "react-bootstrap/Form";
 
 export default function ProfileScreen() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { userInfo } = state;
+  const [name, setName] = useState(userInfo.name);
+  const [email, setEmail] = useState(userInfo.email);
+
+  const submitHandler = async () => {};
+
   return (
     <div className="container small-container">
       <Helmet>
         <title>User Profile</title>
       </Helmet>
+      <h1 className="my-3">User Profile</h1>
+      <form onSubmit={submitHandler}>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            value={email}
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </Form.Group>
+      </form>
     </div>
   );
 }
